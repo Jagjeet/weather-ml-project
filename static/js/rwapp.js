daa91fc90233a6b0349b43ed758cdb6217e998b0
Robert-W2019 <robertresume18@gmail.com>
2021-12-09 20:43:00 -0800

    mlpage update

--- static/js/rwapp.js
+++ static/js/rwapp.js
@@ -13,7 +13,7 @@ function initWeather() {
     let selector = d3.select("#select-station-id");
 
     // Use hardcode selector value
-    let stationId = "690150"
+    //let stationId = "690150"
 
     console.log(startDate);
     console.log(endDate);
