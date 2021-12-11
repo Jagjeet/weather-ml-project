94d26e4542d837e5ea1b334a908d049dadaa76dd
Robert-W2019 <robertresume18@gmail.com>
2021-12-10 18:29:53 -0800

    jwapp.js try and pull station ID again

--- static/js/rwapp.js
+++ static/js/rwapp.js
@@ -164,6 +164,36 @@ function updateStationInfo(data) {
     .text(`Latitude: ${data[0]['LON']}`);
 
 }
+
+function updatelocation(err, rows) {
+  function unpack(rows, key) {
+      return rows.map(function (row) {
+          return row[key];
+      });
+  }
+
+  var data = [
+      {
+          type: "scattermapbox",
+          text: unpack(rows, "#select-station-id"),
+          lon: unpack(rows, "LON"),
+          lat: unpack(rows, "LAT"),
+          marker: { color: "fuchsia", size: 4 }
+      }
+  ];
+
+  var layout = {
+      dragmode: "zoom",
+      mapbox: { style: "open-street-map", center: { lat: 38, lon: -90 }, zoom: 3 },
+      margin: { r: 0, t: 0, b: 0, l: 0 }
+  };
+
+  Plotly.newPlot("myDiv", data, layout);
+}
+
+
+
+
 function weatherBallon( cityID ) {
     let startDate = '2001-01-01';
     let endDate = '2018-12-01';
