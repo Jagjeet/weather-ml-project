53e06574a5260601876e9ac5bd8bbafcb410f614
Robert-W2019 <robertresume18@gmail.com>
2021-12-11 09:36:43 -0800

    openweather

--- static/js/rwapp.js
+++ static/js/rwapp.js
@@ -299,7 +299,7 @@ window.onload = function weatherBallon(cityID) {
 
 
 
-function drawWeather(d) {
+window.onload = function drawWeather(d) {
   var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
   var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
   var description = d.weather[0].description;
